d05c024060f731b50152a7a3365e075c0a678a57
Rahul <rahulkrish321123@gmail.com>
2023-05-23 23:56:29 +0530

    Home page mobile and Desktop complete

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import "./css files/main.css"
+import {startcontrol} from "./js files/startPage";
+
+const search  = document.getElementById("start_search_container");
+search.addEventListener("click",startcontrol.unhide)
---
+++ src/js files/startPage.js
@@ -0,0 +1,28 @@
+const startcontrol = (()=>{
+
+    const unhide =()=>{
+        const search_input=document.getElementById("start_form_input");
+        const search_submit = document.getElementById("start_form_submit");
+        search_submit.addEventListener("click",redirect);
+        search_input.classList.remove("hide");
+        search_submit.classList.remove("hide");
+    }
+
+    const redirect = (e) =>{
+        const search_input=document.getElementById("start_form_input");
+        e.preventDefault();
+        const location_value = search_input.value;
+        document.location.replace("google.com");
+        //function call
+
+    }
+
+    return{
+        unhide,
+
+    }
+})()
+
+export {
+    startcontrol
+}
